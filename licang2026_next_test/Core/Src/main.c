bd7abed3cdc78afc6e1a02273ee2c724788a6f31
d0nut35 <luoyuxuan0127@163.com>
2026-09-23 20:05:48 +0800

    增加按键GPIO与OLED I2C配置

--- licang2026_next_test/Core/Src/main.c
+++ licang2026_next_test/Core/Src/main.c
@@ -20,6 +20,7 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include "dma.h"
+#include "i2c.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -120,6 +121,7 @@ int main(void)
   MX_USART6_UART_Init();
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
 #if MULT_UART_BAREMETAL_TEST_ENABLED
